da7cace23b5d94eb7faa9435ee34165008348fd7
andrew kim <kdk0063@gmail.com>
2024-01-22 15:12:51 -0500

    inital commit with tailwind and proptypes update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Components/Button";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <>  
+            <div>
+                <Button success rounded outline>Click me!</Button>
+            </div>
+            <div>
+                <Button danger outline>Buy Now!</Button>
+            </div>
+            <div>
+                <Button warning>Seal Deal!</Button>
+            </div>
+            <div>
+                <Button secondary outline>Hide Ads!</Button>
+            </div>
+            <div>
+                <Button secondary rounded>Bye Bye!</Button>
+            </div>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,39 @@
+import propTypes from 'prop-types';
+
+function Button({ 
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    rounded,
+    outline
+}){
+    return (
+        <>
+            <button>
+                { children }
+            </button>
+        </>   
+    )
+}
+
+Button.propTypes = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger}) =>{
+        const count = 
+        Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!warning)
+        + Number(!!success)
+        + Number(!!danger);
+
+        if (count > 1){
+            return new Error(
+                'Only one of primary, secondary, success, warning, danger can be true'
+            );
+        }
+    },
+};
+
+export default Button;
