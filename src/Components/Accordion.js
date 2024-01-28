fbbfa01d1b370478497066e529535f1b95c80527
andrew kim <kdk0063@gmail.com>
2024-01-28 16:19:36 -0500

    update accordion component

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+
 function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(-1);
+
+    const handleClick = (nextIndex) => {
+        expandedIndex === nextIndex 
+            ? setExpandedIndex(-1)
+            : setExpandedIndex(nextIndex) 
+    };
+
     const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const icon = (
+            <span className="text-2xl">
+                { isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
+        );
+
         return (
             <div key={index}>
-                <div>
+                <div 
+                    className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" 
+                    onClick={() => handleClick(index)}>
                     { item.label }
+                    { icon }
                 </div>
-                <div>
-                    { item.content }
-                </div>
+                { isExpanded && <div className="border-b p-5">{item.content}</div> }
             </div>
         )
     })
 
     return (
-        <div>
+        <div className="border-x border-t rounded">
             { renderedItems }
         </div>
     )
