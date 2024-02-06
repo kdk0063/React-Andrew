5767d7c7e54e10c0f26bce47ec00882751705013
andrew kim <kdk0063@gmail.com>
2024-02-06 16:35:48 -0500

    WIP age calculator validation

--- src/Pages/AgeCalculator.js
+++ src/Pages/AgeCalculator.js
@@ -5,90 +5,130 @@ import ArrowButton from '../images/svg/icon-arrow.svg'
 import './AgeCalculator.css';
 
 function AgeCalculator() {
-    const [inputDate, setInputDate] = useState('');
-    const [inputMonth, setInputMonth] = useState('');
-    const [inputYear, setInputYear] = useState('');
-    const [ageDifference, setAgeDifference] = useState([]);
+    const [inputDate, setInputDate] = useState('--');
+    const [inputMonth, setInputMonth] = useState('--');
+    const [inputYear, setInputYear] = useState('--');
+    const [ageCalculate, setAgeCalculate] = useState([]);
+    const [isDateValid, setIsDateValid] = useState(false);
+    const [isMonthValid, setIsMonthValid] = useState(false);
+    const [isYearValid, setIsYearValid] = useState(false);
+
 
     const date = new Date();
 
+    const isValidDate = (inputDate) => {
+        const isDate = parseInt(inputDate);
+        return isDate >= 1 && isDate <= new Date(inputYear, inputMonth, 0).getDate();
+    }
+
+    const isValidMonth = (inputMonth) => {
+        const isMonth = parseInt(inputMonth);
+        return isMonth >= 1 && isMonth <= 12;
+    }
+
+    const isValidYear = (inputYear) => {
+        const isYear = parseInt(inputYear);
+        return isYear <= date.getFullYear();
+    };
+
+    const handleChange = (inputValue, pram) => {
+        if(pram === 'date'){
+            setIsDateValid(isValidDate(inputValue))
+            setInputDate(inputValue)   
+        };
+
+        if(pram === 'months'){
+            setIsMonthValid(isValidMonth(inputValue))
+            setInputMonth(inputValue)            
+        }
+
+        if(pram === 'year'){
+            setIsYearValid(isValidYear(inputValue)) 
+            setInputYear(inputValue)                
+        };        
+    }
+
     const handleAgeCalculate = (e) => {
         e.preventDefault();
+        
+        console.log('valid', isDateValid, isMonthValid, isYearValid)
 
-        var dd = date.getDate();
-        var mm = date.getMonth() + 1;
-        var yyyy = date.getFullYear();
+        const birthday = new Date(inputYear, inputMonth-1, inputDate);
+        let years = date.getFullYear() - birthday.getFullYear();
+        let months = date.getMonth() - birthday.getMonth()
+        let days = date.getDate() - birthday.getDate();
+        
+        if (days < 0) {
+            const previousMonth = new Date(date.getFullYear(), date.getMonth(), 0);
+            days += previousMonth.getDate();
+            months--;
+        };
 
-        if (dd < 10) {
-            dd = '0' + dd;
-        }
+        console.log('months', months)
 
-        if (mm < 10) {
-            mm = '0' + mm;
-        }
-        const dDate = Math.abs(dd - inputDate);
-        const dMonth = Math.abs(mm - inputMonth);
-        const dYear = yyyy - inputYear;
-
-        setAgeDifference([dDate, dMonth, dYear]);
+        if (months < 0){
+            months = (12 + months)%12;
+            years--;
+        };
+         
+        setAgeCalculate([days, months, years]);
+        console.log('ageCalculate', ageCalculate)
     }
 
     return (
         <div className="dateCounterPage">
-            <div className="inputCard novalidate" >
+            <div className="input-card">
                 <div className="input-container flex flex-wrap gap-4 lg:gap-8 md:mt-6 w-12/12 lg:w-9/12 md:justify-center lg:justify-start">
-                    <div className="inputWraper">
-                        <label className="inputLable">DAY</label>
+                    <div className="inputWrapper">
+                        <label className="input-label">DAY</label>
                         <input
                             type="text" id="day" placeholder="DD"
-                            className="inputStyle"
-                            max="31"
-                            onChange={e => setInputDate(e.target.value)} />
-                        <span className="validation" id="ErrorDay"></span>
+                            className="input-style"
+                            onChange={e => handleChange(e.target.value, 'date')} />
+                        { !isDateValid && <span className="validation" id="ErrorDay">Need Valid Date</span>}
                     </div>
-                    <div className="inputWraper">
-                        <label className="inputLable">MONTH</label>
+                    <div className="inputWrapper">
+                        <label className="input-label">MONTH</label>
                         <input
                             type="text" id="month" placeholder="MM"
-                            className="inputStyle"
-                            max="12"
-                            onChange={e => setInputMonth(e.target.value)} />
-                        <span className="validation" id="ErrorMonth"></span>
+                            className="input-style"
+                            onChange={e => handleChange(e.target.value, 'months')} />
+                        { !isMonthValid && <span className="validation" id="ErrorMonth">Need Valid Month</span>}
                     </div>
-                    <div className="inputWraper">
-                        <label className="inputLable">YEAR</label>
+                    <div className="inputWrapper">
+                        <label className="input-label">YEAR</label>
                         <input
                             type="text" id="year" placeholder="YYYY"
-                            className="inputStyle"
-                            max={new Date().getFullYear()}
-                            onChange={e => setInputYear(e.target.value)} />
-                        <span className="validation" id="ErrorYear"></span>
+                            className="input-style"
+                            onChange={e => handleChange(e.target.value, 'year')} />
+                        { !isYearValid && <span className="validation" id="ErrorYear">Need Valid Year</span>}
                     </div>
                 </div>
 
                 <div className="special relative flex items-center my-12 sm:my-16 lg:my-0">
                     <hr className="border-none h-px bg-gray-300 w-full lg:flex-grow" />
                     <button
+                        disabled={!isDateValid || !isMonthValid || !isYearValid}
                         className="absolute left-1/2 transform -translate-x-1/2 bg-transparent lg:relative lg:left-auto lg:transform-none" onClick={e => handleAgeCalculate(e)}>
                         <img src={ArrowButton} alt="click button"
-                            className="w-14 sm:w-20 lg:w-32 submit-arrow bg-[#8751fa] rounded-full text-white p-4 sm:p-6 lg:p-7 cursor-pointer transition duration-500 ease-in-out transform hover:bg-black hover:shadow-lg lg:static"></img>
+                            className="sm:w-20 lg:w-32 submit-arrow bg-[#8751fa] rounded-full text-white p-4 sm:p-6 lg:p-7 cursor-pointer transition duration-500 ease-in-out transform hover:bg-black hover:shadow-lg lg:static"></img>
                     </button>
                 </div>
 
-                <div class="age-display text-left md:p-0  lg:p-2 flex flex-col items-start gap-1">
-                    <span class="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
-                        <span class="numbers-f text-[#8751fa] text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
-                            id="years">{ageDifference[2] || "--"}</span>
+                <div className="age-display text-left md:p-0  lg:p-2 flex flex-col items-start gap-1">
+                    <span className="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
+                        <span className="numbers-f text-[#8751fa] text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
+                            id="years">{ageCalculate[2] || '--'}</span>
                         years
                     </span>
-                    <span class="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
-                        <span class="numbers-f text-[#8751fa] text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
-                            id="months">{ageDifference[1] || "--"}</span> months
+                    <span className="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
+                        <span className="numbers-f text-[#8751fa] text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
+                            id="months">{ageCalculate[1] || '--' }</span> months
                     </span>
-                    <span class="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
-                        <span class="numbers-f text-[#8751fa]  text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
-                            id="days">{ageDifference[0] || "--"}</span>
-                        days
+                    <span className="text-f text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
+                        <span className="numbers-f text-[#8751fa]  text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl"
+                            id="days">{ageCalculate[0] || '--'}</span>
+                        days 
                     </span>
                 </div>
 
